5c74aa5ad525ab2e84c5e8b383ff65564b0c5226
hassanbassam2005 <HASSANBASSAM2005@GMAIL.COM>
2025-01-11 19:39:01 +0200

    add comments

--- MetaKit/lib/tuple.h
+++ MetaKit/lib/tuple.h
@@ -16,59 +16,107 @@ using namespace helper_;
 
 namespace tup
 {
-	template<typename... elements>
-	struct tuple { constexpr tuple() = default; };
+    /**
+     * @brief Base case for an empty tuple.
+     *
+     * Defines an empty tuple structure for recursive tuple definition.
+     */
+    template<typename... elements>
+    struct tuple { constexpr tuple() = default; };
 
-	template<typename element1, typename ... element2>
-	struct tuple<element1, element2...> : tuple < element2...>
-	{	
-		explicit constexpr tuple(element1 e1, element2... rest) : tuple<element2...>(rest...), data(e1) {}
-		element1 data;
-	};
+    /**
+     * @brief Recursive tuple definition to store a sequence of elements.
+     *
+     * @tparam element1 The first element type in the tuple.
+     * @tparam element2... Remaining element types in the tuple.
+     */
+    template<typename element1, typename ... element2>
+    struct tuple<element1, element2...> : tuple<element2...>
+    {
+        /**
+         * @brief Constructs a tuple with the given elements.
+         *
+         * @param e1 The first element in the tuple.
+         * @param rest The remaining elements in the tuple.
+         */
+        explicit constexpr tuple(element1 e1, element2... rest)
+            : tuple<element2...>(rest...), data(e1) {}
 
-	//deduction guide for making template argument deduction for constructors work (c++17)
-	template<typename T, typename ...Ts>
-	tuple(T e1, Ts... e2) -> tuple<std::unwrap_ref_decay_t<T>, std::unwrap_ref_decay_t<Ts>...>;
+        element1 data; ///< Stores the data for the current tuple element.
+    };
 
-	template<typename ... elements>
-	constexpr auto make_tuple(elements&&... elem)
-	{
-		return tuple < std::unwrap_ref_decay_t<elements>...> {std::forward<elements>(elem)...};
-	}
+    /**
+     * @brief Deduction guide for template argument deduction for tuple constructors.
+     *
+     * @tparam T The first element type.
+     * @tparam Ts... Remaining element types.
+     */
+    template<typename T, typename ...Ts>
+    tuple(T e1, Ts... e2) -> tuple<std::unwrap_ref_decay_t<T>, std::unwrap_ref_decay_t<Ts>...>;
 
-	namespace detail
-	{
-		template<size_t i, typename Tuple>
-		struct get_impl : get_impl<i - 1, pop_front_t<Tuple>> {};
+    /**
+     * @brief Factory function to create a tuple.
+     *
+     * @param elem The elements to be included in the tuple.
+     * @return A tuple containing the provided elements.
+     */
+    template<typename ... elements>
+    constexpr auto make_tuple(elements&&... elem)
+    {
+        return tuple<std::unwrap_ref_decay_t<elements>...>{std::forward<elements>(elem)...};
+    }
 
-		template <typename Tuple>
-		struct get_impl<0, Tuple>
-		{
-			template<typename T>
-			constexpr static decltype(auto) get(T& t)
-			{
-				constexpr bool is_constant = std::is_const_v<T>;
+    namespace detail
+    {
+        /**
+         * @brief Recursive implementation for accessing tuple elements by index.
+         *
+         * @tparam i The index of the element to access.
+         * @tparam Tuple The tuple type being accessed.
+         */
+        template<size_t i, typename Tuple>
+        struct get_impl : get_impl<i - 1, pop_front_t<Tuple>> {};
 
-				if constexpr (is_constant)
-					return static_cast<const Tuple&>(t).data;
-				else
-					return static_cast<Tuple&>(t).data;
-			}
-		};	
-	}
+        /**
+         * @brief Specialization for accessing the first element of a tuple.
+         *
+         * @tparam Tuple The tuple type being accessed.
+         */
+        template <typename Tuple>
+        struct get_impl<0, Tuple>
+        {
+            /**
+             * @brief Retrieves the element at the specified index.
+             *
+             * @param t The tuple instance.
+             * @return The element at the specified index.
+             */
+            template<typename T>
+            constexpr static decltype(auto) get(T& t)
+            {
+                constexpr bool is_constant = std::is_const_v<T>;
 
-	/*
-	*
-	* get to get the index of the type of the tuple
-	*
-	*/
-	template<size_t i, typename Tuple>
-	constexpr decltype(auto) get(Tuple&& tuple)
-	{
-		return detail::get_impl<i, std::remove_cvref_t<Tuple>>::get(std::forward<Tuple>(tuple));
-	}
+                if constexpr (is_constant)
+                    return static_cast<const Tuple&>(t).data;
+                else
+                    return static_cast<Tuple&>(t).data;
+            }
+        };
+    }
+
+    /**
+     * @brief Retrieves the element at the specified index in the tuple.
+     *
+     * @tparam i The index of the element.
+     * @tparam Tuple The tuple type being accessed.
+     * @param tuple The tuple instance.
+     * @return The element at the specified index.
+     */
+    template<size_t i, typename Tuple>
+    constexpr decltype(auto) get(Tuple&& tuple)
+    {
+        return detail::get_impl<i, std::remove_cvref_t<Tuple>>::get(std::forward<Tuple>(tuple));
+    }
 }
-
-
 
 #endif
